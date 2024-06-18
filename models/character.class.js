3573ced5b5f650a0d130df98e8f75d9ef7640252
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-06-18 17:54:45 +0200

    character walks left and right

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
   height = 300;
   x = 50;
   y = 130;
+  speed = 10;
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -23,13 +24,24 @@ class Character extends MovableObject {
   animate() {
     setInterval(() => {
       if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        // Walk animation
         let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
         // i = 0, 1, 2, 3, 4, 5, 6, 0 , 1, 2, 3, ...
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
       }
-    }, 100);
+    }, 50);
   }
 
   jump() {}
