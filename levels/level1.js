073d4c6e88044e87a25b44a3f0cd4d4961b24529
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-06-18 19:01:40 +0200

    level files created

---
+++ levels/level1.js
@@ -0,0 +1,55 @@
+const level1 = new Level(
+  // prettier-ignore
+  [
+    new Chicken(), 
+    new Chicken(), 
+    new Chicken()
+],
+  // prettier-ignore
+  [
+    new Cloud()
+],
+  [
+    // prettier-ignore
+    new BackgroundObject("img/5_background/layers/air.png", -719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
+
+    new BackgroundObject("img/5_background/layers/air.png", 0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/air.png", 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
+    new BackgroundObject(
+      "img/5_background/layers/3_third_layer/1.png",
+      719 * 2
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/2_second_layer/1.png",
+      719 * 2
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/1_first_layer/1.png",
+      719 * 2
+    ),
+    new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
+    new BackgroundObject(
+      "img/5_background/layers/3_third_layer/2.png",
+      719 * 3
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/2_second_layer/2.png",
+      719 * 3
+    ),
+    new BackgroundObject(
+      "img/5_background/layers/1_first_layer/2.png",
+      719 * 3
+    ),
+  ]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -1,55 +1,9 @@
 class World {
   character = new Character();
   // prettier-ignore
-  enemies = [
-      new Chicken(),
-      new Chicken(),
-      new Chicken()
-      ];
-
-  clouds = [new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/air.png", -719),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/air.png", 719),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
-    new BackgroundObject(
-      "img/5_background/layers/3_third_layer/1.png",
-      719 * 2
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/2_second_layer/1.png",
-      719 * 2
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/1_first_layer/1.png",
-      719 * 2
-    ),
-    new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
-    new BackgroundObject(
-      "img/5_background/layers/3_third_layer/2.png",
-      719 * 3
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/2_second_layer/2.png",
-      719 * 3
-    ),
-    new BackgroundObject(
-      "img/5_background/layers/1_first_layer/2.png",
-      719 * 3
-    ),
-  ];
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObjects;
   canvas;
   ctx;
   keyboard;
