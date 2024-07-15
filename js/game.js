d970ac50c10d7d5f5c1539b896ccdd0ef24e5d3d
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-07-15 10:26:45 +0200

    music toggle button added

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
+let music = new Audio("audio/music.mp3");
+
 async function init() {
   document.querySelector(".start-screen").style.display = "none";
   document.getElementById("canvas").style.display = "block";
@@ -49,3 +51,14 @@ window.addEventListener(
 
 // Add event listener for resize to handle window size changes
 window.addEventListener("resize", checkScreenHeightAndRequestFullscreen);
+
+function toggleMusic() {
+  let musicBtn = document.querySelector(".music-btn");
+  if (music.paused) {
+    music.play();
+    musicBtn.src = "img/music-on.png";
+  } else {
+    music.pause();
+    musicBtn.src = "img/music-off.png";
+  }
+}
